eae9e7a634bfc01eabe17c7f4a88e54491b427f4
Samarth Savanur <samarthsavanur@live.com>
2015-07-15 08:10:13 -0400

    Started Java 8 Streams Workout

---
+++ src/main/java/myStreams.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ssavanu on 7/15/2015.
+ */
+public class myStreams {
+
+    public static int smokeMe (int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/myStreamsTests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ssavanu on 7/15/2015.
+ */
+public class myStreamsTests {
+
+    @Test
+    public void myStream() {
+        assertEquals(4, myStreams.smokeMe(3, 1));
+    }
+}
