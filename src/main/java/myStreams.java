dd12b32591d50ed5f45817c1f97bb274e9d60b89
Samarth Savanur <samarthsavanur@live.com>
2015-07-15 22:45:41 -0400

    J8S 001: Samarth Savanur - Added Stream Class for Fizz Buzz

--- src/main/java/myStreams.java
+++ src/main/java/myStreams.java
@@ -1,9 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Created by Ssavanu on 7/15/2015.
  */
 public class myStreams {
 
-    public static int smokeMe (int a, int b) {
-        return a + b;
+    private static Map<Integer, String> myFizzBuzzMap = new HashMap<Integer, String>();
+    static {
+        myFizzBuzzMap.put(3, "Fizz");
+        myFizzBuzzMap.put(5, "Buzz");
     }
+
+    public static List generateFizzBuzz (int number1, int number2) {
+        List< String > myList = new ArrayList<>();
+
+        return IntStream.rangeClosed(number1, number2)
+                .mapToObj(i -> generateFizzBuzz(i))
+                .collect(Collectors.toList());
+    }
+
+    public static String generateFizzBuzz () {
+        return null;
+    }
+
+    public static String getMyFizzBuzz(int number) {
+        String result =  Optional.of(number)
+                .map(n -> (n % 3 == 0 ? myFizzBuzzMap.get(3) : "") + (n % 5 == 0 ? myFizzBuzzMap.get(5) : ""))
+                .get();
+
+        return result.isEmpty() ? Integer.toString(number) : result;
+    }
+
 }
--- src/test/java/myStreamsTests.java
+++ src/test/java/myStreamsTests.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -8,7 +11,82 @@ import static org.junit.Assert.assertEquals;
 public class myStreamsTests {
 
     @Test
-    public void myStream() {
-        assertEquals(4, myStreams.smokeMe(3, 1));
+    public void whenStreamHasRangeOneToTwo() {
+        String expResult[] = new String[]{"1","2"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 2));
     }
+
+    @Test
+    public void whenStreamHasRangeOneToFour() {
+        String expResult[] = new String[]{"1","2","Fizz","4"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 4));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToFive() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 5));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToSix() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz", "Fizz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 6));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToNine() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz", "Fizz","7","8","Fizz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 9));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToTen() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz", "Fizz","7","8","Fizz", "Buzz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 10));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToTwelve() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz", "Fizz","7","8","Fizz", "Buzz", "11", "Fizz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 12));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToFifteen() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz", "Fizz","7","8","Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 15));
+    }
+
+    @Test
+    public void whenStreamHasRangeOneToTwenty() {
+        String expResult[] = new String[]{"1","2","Fizz","4", "Buzz", "Fizz","7","8","Fizz", "Buzz", "11",
+                                            "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+        List expectedResult = Arrays.asList(expResult);
+        assertEquals(expectedResult, myStreams.generateFizzBuzz(1, 20));
+    }
+
+    @Test
+    public void whenStreamHasNoRange() {
+        String expResult = null;
+        assertEquals(expResult, myStreams.generateFizzBuzz());
+    }
+
+//    @Test
+//    public void whenStreamHasOneNumberToCheck() {
+//        String expResult = "Fizz";
+//        assertEquals(expResult, myStreams.generateFizzBuzz(3));
+//    }
+
+
+
+
 }
